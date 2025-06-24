960d9e724f583256923101320f2109e11830bf81
Noël Sigmunczyk <65312133+linlogge@users.noreply.github.com>
2025-06-24 13:57:11 +0200

    initialize

---
+++ lib/bottom_nav.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import "package:flutter_feather_icons/flutter_feather_icons.dart";
+
+class Navbar extends StatelessWidget {
+  const Navbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(items: [
+      BottomNavigationBarItem(icon: Icon(FeatherIcons.home), label: 'Home'),
+      BottomNavigationBarItem(icon: Icon(FeatherIcons.pieChart), label: 'Search'),
+    ]);
+  }
+}
