1c8758417b3094a34ec5e66f1dc41248eece57d2
Noël Sigmunczyk <65312133+linlogge@users.noreply.github.com>
2025-06-28 18:20:08 +0200

    Integrate fontawesome

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
     resolve: {
         alias: {
             '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+            '~font-awesome': path.resolve(__dirname, 'node_modules/@fortawesome/fontawesome-free'),
         }
     },
 
