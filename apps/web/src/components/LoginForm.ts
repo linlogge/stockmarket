87f5e777bdf400b73dea4ca6cb2994c3b3f0eebd
Noël Sigmunczyk <65312133+linlogge@users.noreply.github.com>
2025-07-01 12:45:15 +0200

    Web: Align with new password from backend

--- apps/web/src/components/LoginForm.ts
+++ apps/web/src/components/LoginForm.ts
@@ -15,7 +15,7 @@ export const LoginForm = () => {
                     </div>
                     <div class="mb-3">
                         <label for="password" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="password" value="password" required>
+                        <input type="password" class="form-control" id="password" value="password123" required>
                     </div>
                     <button type="submit" class="btn btn-primary">Login</button>
                 </form>
